a6f4a3749debf3223421d5bffd02f07ce795c16b
Jore Arteaga <jorge.aranibar26@gmail.com>
2023-05-03 15:02:42 -0500

    added some responsivness

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 
@@ -7,18 +6,9 @@ function App() {
     const [winner, setWinner] = useState<number | null>(null);
     const [concursantes, setConcursantes] = useState<number | null>(null);
     return (
-        <>
+        <div className="myapp">
             <div>
-                <a href="https://vitejs.dev" target="_blank">
-                    <img src={viteLogo} className="logo" alt="Vite logo" />
-                </a>
-                <a href="https://react.dev" target="_blank">
-                    <img
-                        src={reactLogo}
-                        className="logo react"
-                        alt="React logo"
-                    />
-                </a>
+                <img src={viteLogo} className="logo" alt="Vite logo" />
             </div>
             <h1>Sorteo</h1>
             <div className="card">
@@ -43,14 +33,12 @@ function App() {
                 >
                     {!winner ? "Sortear" : `El ganador es ${winner}`}
                 </button>
-                <p>
-                    Edit <code>src/App.tsx</code> and save to test HMR
-                </p>
+                <p>Mucha Suerte!</p>
             </div>
             <p className="read-the-docs">
                 Click on the Vite and React logos to learn more
             </p>
-        </>
+        </div>
     );
 }
 
